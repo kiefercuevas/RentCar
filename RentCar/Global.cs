8479acdc5e654ba7f7292715ed0326179bb9d78f
kiefercuevas <kiefercg2014@gmail.com>
2019-01-17 22:50:16 -0400

    se añadio le clase global donde estaran las variables de session de la applicacion

---
+++ RentCar/Global.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RentCar
+{
+    public static class Global
+    {
+        public static Dictionary<string, string> Variables = new Dictionary<string, string>();
+    }
+}
